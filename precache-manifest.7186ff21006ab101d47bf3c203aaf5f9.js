b05263dbbad274812a3d17b1bebbf7eac2ada632
Rachel Fenichel <fenichr@gmail.com>
2020-07-04 22:04:48 -0700

    Updates

---
+++ precache-manifest.7186ff21006ab101d47bf3c203aaf5f9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e11b6baea353ac9ff48c2f3531b4c561",
+    "url": "/mask-pattern-generator/index.html"
+  },
+  {
+    "revision": "6829483898c3077e2915",
+    "url": "/mask-pattern-generator/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "de9320fc62cdf50eb8e3",
+    "url": "/mask-pattern-generator/static/css/main.2758f41e.chunk.css"
+  },
+  {
+    "revision": "6829483898c3077e2915",
+    "url": "/mask-pattern-generator/static/js/2.5f842145.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/mask-pattern-generator/static/js/2.5f842145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de9320fc62cdf50eb8e3",
+    "url": "/mask-pattern-generator/static/js/main.e014ff76.chunk.js"
+  },
+  {
+    "revision": "aa79edeb61d59ad695ef",
+    "url": "/mask-pattern-generator/static/js/runtime-main.3f0d6445.js"
+  },
+  {
+    "revision": "7ac6b580af340b021d7b989d49327b79",
+    "url": "/mask-pattern-generator/static/media/face_mask_earloop.7ac6b580.jpg"
+  },
+  {
+    "revision": "e6748e962505f5ba17f8862d060513e1",
+    "url": "/mask-pattern-generator/static/media/face_mask_laces.e6748e96.jpg"
+  },
+  {
+    "revision": "e8c859cdfe6166a7b2251fe053069ed3",
+    "url": "/mask-pattern-generator/static/media/face_points_labels_2.e8c859cd.jpg"
+  },
+  {
+    "revision": "6b16100e3d53b868b4078a3cac150751",
+    "url": "/mask-pattern-generator/static/media/face_points_lines2.6b16100e.jpg"
+  },
+  {
+    "revision": "84efa164683b529e8cb75dd02781f578",
+    "url": "/mask-pattern-generator/static/media/face_points_lines_labels2.84efa164.jpg"
+  }
+]);
