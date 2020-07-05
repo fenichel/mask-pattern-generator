8d82f762b6e326749e2540b9c80f204981fffb27
Rachel Fenichel <fenichr@gmail.com>
2020-07-04 17:55:13 -0700

    Updates

---
+++ precache-manifest.e9de4d2ba8c385e7c39958a18b51328f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97dd9419fbffdfdbcf9b3edf2c0f40b2",
+    "url": "/mask-pattern-generator/index.html"
+  },
+  {
+    "revision": "1b02cf9a16e965fd8c89",
+    "url": "/mask-pattern-generator/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c65b49c4f491a7a9883c",
+    "url": "/mask-pattern-generator/static/css/main.2758f41e.chunk.css"
+  },
+  {
+    "revision": "1b02cf9a16e965fd8c89",
+    "url": "/mask-pattern-generator/static/js/2.1d008ddc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/mask-pattern-generator/static/js/2.1d008ddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c65b49c4f491a7a9883c",
+    "url": "/mask-pattern-generator/static/js/main.880f12e6.chunk.js"
+  },
+  {
+    "revision": "aa79edeb61d59ad695ef",
+    "url": "/mask-pattern-generator/static/js/runtime-main.3f0d6445.js"
+  }
+]);
