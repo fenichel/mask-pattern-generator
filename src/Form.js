f2513daaf6f7bacfc2aacc6eb4cc2b61af085d0b
Andrew n marshall <Anm@anm.me>
2020-07-07 23:00:33 -0700

    New form layout With a placeholder for the unit selector

--- src/Form.js
+++ src/Form.js
@@ -48,57 +48,67 @@ class MyForm extends React.Component {
                                     fluid="lg" />
                             </Col>
                             <Col>
-                                <NameInput
-                                    id="patternName"
-                                    label="Name"
-                                    val={this.state.patternName}
-                                    onValueChange={this.handleInputChange}>
-                                </NameInput>
-                                <DimensionInput
-                                    id="earToBridge"
-                                    label="Ear top to nose bridge (A to B)"
-                                    val={this.state.earToBridge}
-                                    onValueChange={this.handleInputChange}>
-                                </DimensionInput>
-                                <DimensionInput
-                                    id="earToNose"
-                                    label="Ear top to nose tip (A to C)"
-                                    val={this.state.earToNose}
-                                    onValueChange={this.handleInputChange}>
-                                </DimensionInput>
-                                <DimensionInput
-                                    id="bridgeToTip"
-                                    label="Nose bridge to nose tip (B to C)"
-                                    val={this.state.bridgeToTip}
-                                    onValueChange={this.handleInputChange}>
-                                </DimensionInput>
-                            </Col>
-                            <Col>
-                                <DimensionInput
-                                    id="noseToChin"
-                                    label="Nose to chin with mouth open to talk (C to D)"
-                                    val={this.state.noseToChin}
-                                    onValueChange={this.handleInputChange}>
-                                </DimensionInput>
-                                <DimensionInput
-                                    id="chinToThroat"
-                                    label="Chin to throat (D to E)"
-                                    val={this.state.chinToThroat}
-                                    onValueChange={this.handleInputChange}>
-                                </DimensionInput>
-                                <DimensionInput
-                                    id="earToThroat"
-                                    label="Throat to ear bottom (E to F)"
-                                    val={this.state.earToThroat}
-                                    onValueChange={this.handleInputChange}>
-                                </DimensionInput>
-                                <DimensionInput
-                                    id="earHeight"
-                                    label="Ear height (A to F)"
-                                    val={this.state.earHeight}
-                                    onValueChange={this.handleInputChange}>
-                                </DimensionInput>
-
+                                <Row>
+                                    <Col>
+                                        <NameInput
+                                            id="patternName"
+                                            label="Name"
+                                            val={this.state.patternName}
+                                            onValueChange={this.handleInputChange}>
+                                        </NameInput>
+                                    </Col>
+                                    <Col>
+                                        <div>Unit of measure:</div>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <DimensionInput
+                                            id="earToBridge"
+                                            label="Ear top to nose bridge (A to B)"
+                                            val={this.state.earToBridge}
+                                            onValueChange={this.handleInputChange}>
+                                        </DimensionInput>
+                                        <DimensionInput
+                                            id="earToNose"
+                                            label="Ear top to nose tip (A to C)"
+                                            val={this.state.earToNose}
+                                            onValueChange={this.handleInputChange}>
+                                        </DimensionInput>
+                                        <DimensionInput
+                                            id="bridgeToTip"
+                                            label="Nose bridge to nose tip (B to C)"
+                                            val={this.state.bridgeToTip}
+                                            onValueChange={this.handleInputChange}>
+                                        </DimensionInput>
+                                    </Col>
+                                    <Col>
+                                        <DimensionInput
+                                            id="noseToChin"
+                                            label="Nose to chin with mouth open to talk (C to D)"
+                                            val={this.state.noseToChin}
+                                            onValueChange={this.handleInputChange}>
+                                        </DimensionInput>
+                                        <DimensionInput
+                                            id="chinToThroat"
+                                            label="Chin to throat (D to E)"
+                                            val={this.state.chinToThroat}
+                                            onValueChange={this.handleInputChange}>
+                                        </DimensionInput>
+                                        <DimensionInput
+                                            id="earToThroat"
+                                            label="Throat to ear bottom (E to F)"
+                                            val={this.state.earToThroat}
+                                            onValueChange={this.handleInputChange}>
+                                        </DimensionInput>
+                                        <DimensionInput
+                                            id="earHeight"
+                                            label="Ear height (A to F)"
+                                            val={this.state.earHeight}
+                                            onValueChange={this.handleInputChange}>
+                                        </DimensionInput>
+                                    </Col>
+                                </Row>
                             </Col>
                         </Row>
                     </Form>
